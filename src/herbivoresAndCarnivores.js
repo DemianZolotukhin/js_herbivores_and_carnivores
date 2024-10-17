c4d45b61050860814f97aa0451498b88a6f777b6
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-10-17 18:26:59 +0300

    updateMinor

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -20,17 +20,11 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(toBite) {
-    if (toBite instanceof Herbivore) {
-      if (!toBite.hidden && toBite.health > 0) {
-        toBite.health -= 50;
-      }
+    if (toBite instanceof Herbivore && !toBite.hidden) {
+      toBite.health -= 50;
     }
 
-    if (toBite.health <= 0) {
-      const index = Animal.alive.indexOf(toBite);
-
-      Animal.alive.splice(index, 1);
-    }
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
   }
 }
 
